d55b4c09db638565c532acfcb795ba2646fd770e
jirkakrepl <jiri.krepl@gmail.com>
2023-06-25 15:06:05 +0200

    refactor: remove prerender header
    with this header, prerender did not work at all
    
    - also enable logs

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender();
+var server = prerender({ logRequests: true, captureConsoleLog: true });
 
-server.use(prerender.sendPrerenderHeader());
 server.use(prerender.browserForceRestart());
 // server.use(prerender.blockResources());
 server.use(prerender.addMetaTags());
