ec69a00a2266b4bac407fc008a9a8597cc1d8704
jirkakrepl <jiri.krepl@gmail.com>
2023-06-26 12:41:17 +0200

    refactor: disable plugins

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var prerender = require('./lib');
 
 var server = prerender({ logRequests: true, captureConsoleLog: true });
 
-server.use(prerender.browserForceRestart());
+// server.use(prerender.browserForceRestart());
 // server.use(prerender.blockResources());
-server.use(prerender.addMetaTags());
+// server.use(prerender.addMetaTags());
 server.use(prerender.removeScriptTags());
-server.use(prerender.httpHeaders());
-server.use(require('prerender-memory-cache'));
+// server.use(prerender.httpHeaders());
+// server.use(require('prerender-memory-cache'));
 
 server.start();
