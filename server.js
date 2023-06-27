c14748e22a3e906c9816b0668f698ec97bad3789
jirkakrepl <jiri.krepl@gmail.com>
2023-06-27 14:05:52 +0200

    refactor: move chrome options to server.js file
    from chrome.js internal file

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender({ logRequests: true, captureConsoleLog: true });
+/**
+ * use prod. options from here:
+ * https://github.com/prerender/prerender/pull/748
+ *
+ * test chrome startup on MacOS:
+ * /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --disable-gpu --remote-debugging-port=9922 --disable-dev-shm-usage --hide-scrollbars
+ */
+var server = prerender({
+    logRequests: true,
+    captureConsoleLog: true,
+    pageLoadTimeout: 60 * 1000,
+    chromeFlags: [
+        '--no-sandbox',
+        '--headless',
+        '--disable-gpu',
+        '--remote-debugging-port=9222',
+        '--disable-dev-shm-usage',
+        '--hide-scrollbars',
+        // '--enable-logging',
+        // '--v=1',
+        // '--user-data-dir=/home/jirka/chrome-profile' // chrome logs will be saved here
+    ],
+});
 
 // server.use(prerender.browserForceRestart());
 // server.use(prerender.blockResources());
